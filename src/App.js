8c1c307b7640071bd73f010583450a0d1517a5b0
Ivan <baxxulol@gmail.com>
2022-10-26 13:00:45 +0300

    свидетель

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useMemo, useState} from "react";
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import {Container, Row} from "react-bootstrap";
 import Links from "./components/Links";
 import {UserContext} from "./context/UserContext";
 import './App.scss'
 import {CheckUserInLocalStorage} from "./context/CheckUserInLocalStorage";
+import SvidetelLeft from "./img/свидетель left.png";
+import SvidetelRight from "./img/свидетель right.png";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -16,17 +18,27 @@ function App() {
         CheckUserInLocalStorage(context)
     })
 
+    const location = useLocation()
+
     return (
         <UserContext.Provider value={context}>
             <Container>
                 <Row className="mt-3 mb-3">
                     <NavBar/>
                 </Row>
-                <Row className="mt-3 mb-5">
+                <Row className="mt-3 mb-3">
                     <Outlet/>
                 </Row>
-                <Row className="mt-3 mb-3">
+                <Row className="mt-5 mb-5">
                     <Links/>
+                </Row>
+
+                <Row className="mt-3 mb-3">
+                    {location.pathname === '/' ?
+                        <img src={SvidetelLeft} alt="Свидетель left"/>
+                        :
+                        <img src={SvidetelRight} alt="Свидетель right"/>
+                    }
                 </Row>
             </Container>
         </UserContext.Provider>
